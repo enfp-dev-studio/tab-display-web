0900a9e814bc573b2f60011d4015c373f8d5f1d7
Jake Roh <enfpdev@gmail.com>
2023-03-08 16:00:07 +0900

    add privacy

---
+++ src/pages/privacy/index.tsx
@@ -0,0 +1,254 @@
+const Privacy = () => {
+  return (
+    <div className="container m-10 max-w-4xl">
+      <article id="abd2ac45-e088-4c1a-8484-20426a383983" className="page sans">
+        <header>
+          <h1 className="text-3xl font-bold">Privacy Policy</h1>
+        </header>
+        <div className="mt-12">
+          <p id="b7da42e2-660a-45d5-b350-c9fc92d47a42"></p>
+          <p id="8fe2d278-a60b-462e-97ff-09d971b34f53">
+            ENFP-Dev-Studio built the Tab Display app as an Ad Supported app.
+            This SERVICE is provided by ENFP-Dev-Studio at no cost and is
+            intended for use as is.
+          </p>
+          <p id="9a110f14-f3bf-40d8-8a84-7f4fc977f29d">
+            This page is used to inform visitors regarding my policies with the
+            collection, use, and disclosure of Personal Information if anyone
+            decided to use my Service.
+          </p>
+          <p id="03f26358-8f64-4aaf-ae5b-6de76c08e3d6">
+            If you choose to use my Service, then you agree to the collection
+            and use of information in relation to this policy. The Personal
+            Information that I collect is used for providing and improving the
+            Service. I will not use or share your information with anyone except
+            as described in this Privacy Policy.
+          </p>
+          <p id="4e0679db-7958-48e8-8cfa-479403b0d8d5">
+            The terms used in this Privacy Policy have the same meanings as in
+            our Terms and Conditions, which are accessible at Tab Display unless
+            otherwise defined in this Privacy Policy.
+          </p>
+          <p className="mt-4 text-xl" id="2dc01f16-23b5-42d3-b9ad-b30c24c576f6">
+            <strong>Information Collection and Use</strong>
+          </p>
+          <p id="b3e9ae31-d150-4ade-bf89-adb62e7ad2cb" className="mt-4">
+            For a better experience, while using our Service, I may require you
+            to provide us with certain personally identifiable information. The
+            information that I request will be retained on your device and is
+            not collected by me in any way.
+          </p>
+          <p id="6b370bcd-3ce6-4fed-96ec-17cd76a31690">
+            The app does use third-party services that may collect information
+            used to identify you.
+          </p>
+          <p id="7d3149e0-b17f-41b9-8b14-b94e2ea77f52">
+            Link to the privacy policy of third-party service providers used by
+            the app
+          </p>
+          <ul
+            id="73d05b06-79fa-41af-8914-c96100bc57cf"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              <a href="https://www.google.com/policies/privacy/">
+                Google Play Services
+              </a>
+            </li>
+          </ul>
+          <ul
+            id="b75e6c8f-9359-4d7d-ae05-15e99c20b4e8"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              <a href="https://support.google.com/admob/answer/6128543?hl=en">
+                AdMob
+              </a>
+            </li>
+          </ul>
+          <ul
+            id="77313b21-4c69-4b71-8e69-a6007f9b853e"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              <a href="https://firebase.google.com/policies/analytics">
+                Google Analytics for Firebase
+              </a>
+            </li>
+          </ul>
+          <p className="mt-4 text-xl" id="49a3bb33-6cb3-42fa-8d03-1a218e7e83a8">
+            <strong>Log Data</strong>
+          </p>
+          <p id="ced94a06-4c71-45d3-b968-238add60a7c6" className="mt-4">
+            I want to inform you that whenever you use my Service, in a case of
+            an error in the app I collect data and information (through
+            third-party products) on your phone called Log Data. This Log Data
+            may include information such as your device Internet Protocol (“IP”)
+            address, device name, operating system version, the configuration of
+            the app when utilizing my Service, the time and date of your use of
+            the Service, and other statistics.
+          </p>
+          <p className="mt-4 text-xl" id="6de438d0-0213-450b-9dcf-d4b2b6c6d537">
+            <strong>Cookies</strong>
+          </p>
+          <p id="47d93b7c-c7ce-41ce-b57a-2e721cf79a76" className="mt-4">
+            Cookies are files with a small amount of data that are commonly used
+            as anonymous unique identifiers. These are sent to your browser from
+            the websites that you visit and are stored on your device&#x27;s
+            internal memory.
+          </p>
+          <p id="afba6ce0-ccbc-43d8-b97a-6271b0905f3f">
+            This Service does not use these “cookies” explicitly. However, the
+            app may use third-party code and libraries that use “cookies” to
+            collect information and improve their services. You have the option
+            to either accept or refuse these cookies and know when a cookie is
+            being sent to your device. If you choose to refuse our cookies, you
+            may not be able to use some portions of this Service.
+          </p>
+          <p className="mt-4 text-xl" id="801d2a21-4ef1-4ce2-9b87-bae40538d0ee">
+            <strong>Service Providers</strong>
+          </p>
+          <p id="5e0bd603-0416-4e77-9db2-e97b0528b7a7" className="mt-4">
+            I may employ third-party companies and individuals due to the
+            following reasons:
+          </p>
+          <ul
+            id="5e57e5fa-1be9-4b88-bcf8-7efb9c3447d9"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              To facilitate our Service;
+            </li>
+          </ul>
+          <ul
+            id="ce13d72f-8f47-46ee-a065-de25f25af9d8"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              To provide the Service on our behalf;
+            </li>
+          </ul>
+          <ul
+            id="b092db41-02bf-4bcc-9dda-05c453836e65"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              To perform Service-related services; or
+            </li>
+          </ul>
+          <ul
+            id="d1afa78a-2664-4bf2-99e0-dceb358c1da4"
+            className="bulleted-list ml-8 mt-2"
+          >
+            <li
+              style={{
+                listStyleType: "disc",
+              }}
+            >
+              To assist us in analyzing how our Service is used.
+            </li>
+          </ul>
+          <p id="cc1eb3d3-cbc3-42e7-866f-b76cde54337f">
+            I want to inform users of this Service that these third parties have
+            access to their Personal Information. The reason is to perform the
+            tasks assigned to them on our behalf. However, they are obligated
+            not to disclose or use the information for any other purpose.
+          </p>
+          <p className="mt-4 text-xl" id="6a99084e-5589-4884-8ce0-19bbc9e49ffa">
+            <strong>Security</strong>
+          </p>
+          <p id="64f0102c-73b9-4bd0-941c-d45793370e49" className="mt-4">
+            I value your trust in providing us your Personal Information, thus
+            we are striving to use commercially acceptable means of protecting
+            it. But remember that no method of transmission over the internet,
+            or method of electronic storage is 100% secure and reliable, and I
+            cannot guarantee its absolute security.
+          </p>
+          <p className="mt-4 text-xl" id="7162a676-9bc6-4dd8-a2a6-2cbf90052a35">
+            <strong>Links to Other Sites</strong>
+          </p>
+          <p id="3503eea5-f265-42c9-a26c-a9f14f34ba82" className="mt-4">
+            This Service may contain links to other sites. If you click on a
+            third-party link, you will be directed to that site. Note that these
+            external sites are not operated by me. Therefore, I strongly advise
+            you to review the Privacy Policy of these websites. I have no
+            control over and assume no responsibility for the content, privacy
+            policies, or practices of any third-party sites or services.
+          </p>
+          <p className="mt-4 text-xl" id="618c85de-80db-4f92-b326-d4edc87d50be">
+            <strong>Children’s Privacy</strong>
+          </p>
+          <p id="fc362592-b042-4ee8-94ee-ca2e7b377430" className="mt-4">
+            These Services do not address anyone under the age of 13. I do not
+            knowingly collect personally identifiable information from children
+            under 13 years of age. In the case I discover that a child under 13
+            has provided me with personal information, I immediately delete this
+            from our servers. If you are a parent or guardian and you are aware
+            that your child has provided us with personal information, please
+            contact me so that I will be able to do the necessary actions.
+          </p>
+          <p className="mt-4 text-xl" id="7a42d2fc-57fa-4d87-bcb2-d72621fb6ba4">
+            <strong>Changes to This Privacy Policy</strong>
+          </p>
+          <p id="2555f4bb-5a9d-4d4e-9cfa-51e228cdb229" className="mt-4">
+            I may update our Privacy Policy from time to time. Thus, you are
+            advised to review this page periodically for any changes. I will
+            notify you of any changes by posting the new Privacy Policy on this
+            page.
+          </p>
+          <p id="871f5624-5aa7-41d9-b36f-3c950a5d2e63">
+            This policy is effective as of 2023-02-16
+          </p>
+          <p className="mt-4 text-xl" id="c9dae868-de36-4320-a6a9-bf4a876d33a6">
+            <strong>Contact Us</strong>
+          </p>
+          <p id="868d2929-65ee-4a48-aa06-f04fb89e54ad" className="mt-4">
+            If you have any questions or suggestions about my Privacy Policy, do
+            not hesitate to contact me at
+          </p>
+          <br />
+          <a href="mailto:contact@enfpdev.com" className="font-bold underline">
+            contact@enfpdev.com
+          </a>
+          <br />
+          <p id="c3364fb0-1379-4038-873b-d1e74b64ca66" className="mt-4">
+            This privacy policy page was created at
+            <a href="https://privacypolicytemplate.net/">
+              privacypolicytemplate.net
+            </a>
+            and modified/generated by
+            <a href="https://app-privacy-policy-generator.nisrulz.com/">
+              App Privacy Policy Generator
+            </a>
+          </p>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default Privacy;
